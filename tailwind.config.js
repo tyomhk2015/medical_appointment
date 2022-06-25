f1a2d238ab73daca8691779e3878aba2ecf5e070
tyomhk2015 <cheertrain15@naver.com>
2022-06-25 17:43:58 +0900

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Delcaring using Tailwind CSS feature in the following files.
+  // When multiple files are designated, spaces between file extensions will cause an error.
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
