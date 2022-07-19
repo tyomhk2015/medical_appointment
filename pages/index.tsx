bc92d79d1cd75dfe372651a917f70f393c8c0f2f
tyomhk2015 <cheertrain15@naver.com>
2022-07-19 10:19:57 +0900

    Testing remote url

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ const Home: NextPage = () => {
     const deployedURL = "https://medical-appointment-server.herokuapp.com/";
     const localURL = "http://localhost:8080";
 
-    const clientSocket = io(localURL);
+    const clientSocket = io(deployedURL);
 
     setSocket(clientSocket);
 
